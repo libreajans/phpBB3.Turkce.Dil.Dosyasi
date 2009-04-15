a3c0857bf0282100cc89cc1df1af614a07213cdf
angelside <angelside@users.sourceforge.net>
2009-04-15 07:33:04 +0000

    muhtelif düzeltmeler

--- root/language/tr/acp/search.php
+++ root/language/tr/acp/search.php
@@ -41,19 +41,19 @@ $lang = array_merge($lang, array(
 
 	'COMMON_WORD_THRESHOLD'					=> 'Genel kelime başlangıcı',
 	'COMMON_WORD_THRESHOLD_EXPLAIN'			=> 'Tüm iletilerde belli bir orandan fazla bulunan kelimeler yaygın kelime sayılır. Yaygın kelimeler arama sorgularında yok sayılırlar; yok sayılmamaları için "0" yazınız. Bu özellik panodaki ileti sayısı 100 den fazla ise etkili olur. Şu anki yaygın kelimelerin durumunun tekrar değerlendirilmesi için arama indeksini silip yeniden oluşturmalısınız.',
-	'CONFIRM_SEARCH_BACKEND'				=> 'Arama sonuçlarını farklı bir sisteme değiştirmek istediğinze emin misiniz? Arama sistemini değiştirdikten sonra yeni sistem için, bir arama indexi oluşturmanız gerekecek. Eğer eski arama sistemine dönmeyi planlamıyorsanız, sistem kaynaklarınızı boşaltmak için eski arama indexlerini silebilirsiniz.',
+	'CONFIRM_SEARCH_BACKEND'				=> 'Arama sonuçlarını farklı bir sisteme değiştirmek istediğinze emin misiniz? Arama sistemini değiştirdikten sonra yeni sistem için, bir arama indeksi oluşturmanız gerekecek. Eğer eski arama sistemine dönmeyi planlamıyorsanız, sistem kaynaklarınızı boşaltmak için eski arama indekslerini silebilirsiniz.',
 	'CONTINUE_DELETING_INDEX'				=> 'Önceki indeks silme işlemine devam et',
 	'CONTINUE_DELETING_INDEX_EXPLAIN'		=> 'Bir indeks silme işlemi başlatıldı. Arama indeksi sayfasına erişmek için, silme işlemini tamamlamalı veya iptal etmelisiniz.',
 	'CONTINUE_INDEXING'						=> 'Önceki indeks oluşturma işlemine devam et',
-	'CONTINUE_INDEXING_EXPLAIN'				 => 'Bir indeks oluşturma işlemi başlatıldı. Arama indeksi sayfasına erişmek için, indeks oluşturma işlemini tamamlamalı veya iptal etmelisiniz.',
+	'CONTINUE_INDEXING_EXPLAIN'				=> 'Bir indeks oluşturma işlemi başlatıldı. Arama indeksi sayfasına erişmek için, indeks oluşturma işlemini tamamlamalı veya iptal etmelisiniz.',
 	'CREATE_INDEX'							=> 'İndeks oluştur',
 
 	'DELETE_INDEX'							=> 'İndeks sil',
 	'DELETING_INDEX_IN_PROGRESS'			=> 'İndeks siliniyor',
 	'DELETING_INDEX_IN_PROGRESS_EXPLAIN'	=> 'Arama uygulaması indeksi siliniyor. Bu işlem bir kaç dakika sürebilir.',
 
-	'FULLTEXT_MYSQL_INCOMPATIBLE_VERSION'	=> 'MySQL tam metin indekslemesi sadece MySQL4 ve üzeri ile kullanılabilir.',
-	'FULLTEXT_MYSQL_NOT_MYISAM'				=> 'MySQL tam metin indekslemesi sadece MyISAM tablolar ile kullanılabilir.',
+	'FULLTEXT_MYSQL_INCOMPATIBLE_VERSION'	=> 'MySQL tam metin (fulltext) indekslemesi sadece MySQL4 ve üzeri ile kullanılabilir.',
+	'FULLTEXT_MYSQL_NOT_MYISAM'				=> 'MySQL tam metin (fulltext) indekslemesi sadece MyISAM tablolar ile kullanılabilir.',
 	'FULLTEXT_MYSQL_TOTAL_POSTS'			=> 'Kayıt altına alınan mesajların toplam sayısı',
 	'FULLTEXT_MYSQL_MBSTRING'				=> 'Latin olmayan UTF-8 karakterleri için mbstring kullanımı:',
 	'FULLTEXT_MYSQL_PCRE'					=> 'Latin olmayan UTF-8 karakterleri için PCRE kullanımı:',
@@ -79,27 +79,27 @@ $lang = array_merge($lang, array(
 
 	'PROGRESS_BAR'							=> 'Durum çubuğu',
 	
-'SEARCH_GUEST_INTERVAL'					=> 'Misafir kullanıcı arama flood aralığı', // Flood için tercüme ?
+	'SEARCH_GUEST_INTERVAL'					=> 'Misafir kullanıcı arama sıklık aralığı',
 	'SEARCH_GUEST_INTERVAL_EXPLAIN'			=> 'Misafirlerin aramalar arasında beklemesi gereken saniye sayısı. Eğer bir misafir arama yaparsa, tüm misafirler yeni arama yapmak için aramanın üzerinden bu sürenin geçmesini beklemeliler.',
-	'SEARCH_INDEX_CREATE_REDIRECT'			=> '%1$d numaralı iletiye kadar olan tüm iletiler indeksendi. Bu aşamada %2$d ileti vardı.<br />Şu andaki indexleme oranı saniye başına yaklaşık %3$.1f iletidir. <br />İndexleme sürüyor…',
+	'SEARCH_INDEX_CREATE_REDIRECT'			=> '%1$d numaralı iletiye kadar olan tüm iletiler indekslendi. Bu aşamada %2$d ileti vardı.<br />Şu andaki indeksleme oranı saniye başına yaklaşık %3$.1f iletidir. <br />İndeksleme sürüyor…',
 	'SEARCH_INDEX_DELETE_REDIRECT'			=> '%1$d numaralı iletiye kadar tüm iletiler arama indeksinden kaldırıldı. <br />Silme işlemi devam ediyor…',
 	'SEARCH_INDEX_CREATED'					=> 'Pano veritabanındaki tüm mesajlar sorunsuzca indekslendi.',
 	'SEARCH_INDEX_REMOVED'					=> 'Bu uygulama için arama indeksi sorunsuzca silindi.',
-'SEARCH_INTERVAL'						=> 'Kullanıcı arama flood aralığı', // Flood için tercüme ?
+	'SEARCH_INTERVAL'						=> 'Kullanıcı arama sıklık aralığı',
 	'SEARCH_INTERVAL_EXPLAIN'				=> 'İki arama arasında beklenecek saniye cinsinden süre. Bu süre her kullanıcı için bağımsız kontrol edilir.',
 	'SEARCH_STORE_RESULTS'					=> 'Arama sonucu önbellek boyutu',
 	'SEARCH_STORE_RESULTS_EXPLAIN'			=> 'Önbelleğe alınmış arama sonuçlarının saniye cinsinden sona ereceği süre. Eğer arama önbelleğini kapatmak istiyorsanız 0 olarak ayarlayın.',
 	'SEARCH_TYPE'							=> 'Arama uygulaması',
-	'SEARCH_TYPE_EXPLAIN'					=> 'phpBB ileti içeriğindeki yazıyı ararken kullanılacak sisteminizi kendinizin seçmesine olanak tanır. Varsayılan olarak phpBB’nin tümyazıyı arama özelliği kullanılır.',
+	'SEARCH_TYPE_EXPLAIN'					=> 'phpBB ileti içeriğindeki yazıyı ararken kullanılacak sisteminizi kendinizin seçmesine olanak tanır. Varsayılan olarak phpBB’nin tam metin arama özelliği kullanılır.',
 	'SWITCHED_SEARCH_BACKEND'				=> 'Arama uygulamasını değiştirdiniz. Yeni arama uygulamasını kullanabilmek amacıyla uygulama için bir indeks seçtiğinize emin olmalısınız.',
 
 	'TOTAL_WORDS'							=> 'Kayıt altına alınan kelimelerin toplam sayısı',
 	'TOTAL_MATCHES'							=> 'İletilere bağlı olarak kayıt altına alınan kelimenin toplam sayısı',
 
 	'YES_SEARCH'							=> 'Arama özelliğini aktif hale getirir.',
-	'YES_SEARCH_EXPLAIN'					=> '"Kullanıcı Arama" da dahil olmak üzere arama özelliğini aktif hale getirir.',
-	'YES_SEARCH_UPDATE'						=> 'Metin güncellemesini aç',
-	'YES_SEARCH_UPDATE_EXPLAIN'				=> 'Her ileti gönderildiğinde arama indeksini günceller. Eğer arama özelliği kapalıysa güncelleme yapılmaz.',
+	'YES_SEARCH_EXPLAIN'					=> '"Kullanıcı arama" da dahil olmak üzere arama özelliğini aktif hale getirir.',
+	'YES_SEARCH_UPDATE'						=> 'Tam metin güncellemesini aç',
+	'YES_SEARCH_UPDATE_EXPLAIN'				=> 'Her ileti gönderildiğinde tam metin arama indeksi güncellenir. Eğer arama özelliği kapalıysa güncelleme yapılmaz.',
 ));
 
 ?>
